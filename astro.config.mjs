b386c07f0eb61e1b854bb0b9046b8caff0d898c1
Vishnu Venugopal <vishnuvgn05@gmail.com>
2025-11-28 21:03:10 -0500

    define config edited

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://vishnuvgn.github.io', // Your GitHub username URL
+    base: '/wishlist',                   // Your repository name
+});
