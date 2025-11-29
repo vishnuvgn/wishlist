31484421d285727defbc49f3965fb3cda821cf5b
Vishnu Venugopal <vishnuvgn05@gmail.com>
2025-11-28 21:11:16 -0500

    dynamic image finding

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vishnuvgn.github.io', // Your GitHub username URL
-    base: '/wishlist',                   // Your repository name
+    site: 'https://vishnuvgn.github.io',
+    // Only use the subpath when building for production
+    base: process.env.NODE_ENV === 'production' ? '/wishlist' : '/',
 });
